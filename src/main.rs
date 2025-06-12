c3706f4e42ad42ee689776468bbf4ae189e9299d
krasimir-zhelezov <krasizh07@gmail.com>
2025-06-12 21:15:38 +0300

    feat: loop and exit

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use std::io;
 
 fn main() {
-    let mut input = String::new();
+    loop {
+        let mut input = String::new();
 
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
 
-    let input = input.trim();
+        let input = input.trim();
 
-    println!("{}", input);
+        if input == "exit" {
+            break;
+        }
+    }
 }
