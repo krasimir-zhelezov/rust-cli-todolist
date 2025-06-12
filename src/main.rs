c67f451106b9165420461ba41f80fc085c097567
krasimir-zhelezov <krasizh07@gmail.com>
2025-06-12 21:19:20 +0300

    feat: add task function

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,29 @@ fn main() {
 
         if input == "exit" {
             break;
+        } else if input == "add" {
+            add_task();
         }
     }
+}
+
+fn add_task() {
+    println!("Name: ");
+    let mut name = String::new();
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
+
+    let name = name.trim();
+
+    println!("Description: ");
+
+    let mut description = String::new();
+    io::stdin()
+        .read_line(&mut description)
+        .expect("Failed to read line");
+
+    let description = description.trim();
+
+    println!("Task: {}", name);
 }
