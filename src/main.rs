1729b81a93656d0b7551f05a6d43181142edd2f5
krasimir-zhelezov <krasizh07@gmail.com>
2025-06-21 15:15:38 +0300

    feat: added Task struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use std::{collections::HashMap, io::{self, Read}};
 
+#[derive(Debug)]
+struct Task {
+    name: String,
+    description: String,
+    done: bool
+}
+
 fn main() {
-    let mut tasks: Vec<HashMap<String, String>> = Vec::new();
+    let mut tasks: Vec<Task> = Vec::new();
 
     loop {
         let mut input = String::new();
@@ -14,8 +21,8 @@ fn main() {
 
         match input {
             "exit" => break,
-            "add" => add_task(tasks.as_mut()),
-            "view" => view_tasks(tasks.as_mut()),
+            "add" => tasks.push(add_task()),
+            "view" => println!("{:#?}", tasks),
             "delete" => delete_task(tasks.as_mut()),
             "complete" => complete_task(tasks.as_mut()),
             _ => println!("Uknown command"),
@@ -23,7 +30,7 @@ fn main() {
     }
 }
 
-fn add_task(tasks: &mut Vec<HashMap<String, String>>) {
+fn add_task() -> Task {
     println!("Name: ");
     let mut name = String::new();
     io::stdin()
@@ -41,25 +48,23 @@ fn add_task(tasks: &mut Vec<HashMap<String, String>>) {
 
     let description = description.trim();
 
-    let mut task = HashMap::new();
-
-    task.insert("name".to_string(), name.to_string());
-    task.insert("desc".to_string(), description.to_string());
-    task.insert("done".to_string(), false.to_string());
-
-    tasks.push(task);
-}
-
-fn view_tasks(tasks: &mut Vec<HashMap<String, String>>) {
-    for (i, task) in tasks.iter().enumerate() {
-        println!("----- TASK #{} -----", i);
-        println!("name: {}", task.get("name").unwrap());
-        println!("description: {}", task.get("desc").unwrap());
-        println!("done: {}", task.get("done").unwrap());
+    Task {
+        name: name.to_string(),
+        description: description.to_string(),
+        done: false
     }
 }
 
-fn delete_task(tasks: &mut Vec<HashMap<String, String>>) {
+// fn view_tasks(tasks: &mut Vec<HashMap<String, String>>) {
+//     for (i, task) in tasks.iter().enumerate() {
+//         println!("----- TASK #{} -----", i);
+//         println!("name: {}", task.get("name").unwrap());
+//         println!("description: {}", task.get("desc").unwrap());
+//         println!("done: {}", task.get("done").unwrap());
+//     }
+// }
+
+fn delete_task(tasks: &mut Vec<Task>) {
     let mut id = String::new();
     io::stdin()
         .read_line(&mut id)
@@ -70,12 +75,12 @@ fn delete_task(tasks: &mut Vec<HashMap<String, String>>) {
     tasks.remove(id);
 }
 
-fn complete_task(tasks: &mut Vec<HashMap<String, String>>) {
+fn complete_task(tasks: &mut[Task]) {
     let mut id = String::new();
     io::stdin()
         .read_line(&mut id)
         .expect("Failed to read line");
 
     let id: usize = id.trim().parse().expect("Not a valid usize");
-    tasks[id].insert("done".to_string(), true.to_string());
+    tasks[id].done = true;
 }
